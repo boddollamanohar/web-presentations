7cea22a70b135e9c5e7433cb8436439695d4c0f1
unknown <iambmanohar@gmail.com>
2022-11-30 21:33:07 +0530

    data

--- java Script/functions/two.js
+++ java Script/functions/two.js
@@ -1,5 +1,7 @@
-function add(a,b){
-    console.log(a*b)
+function add(a, b, c, d, e, f, f) {
+    console.log(a + b + c + d + e + f + f)
 
 }
-add (10,20)
+add(10, 201, 3, 4, 5, 6, 7,)
+add()
+
--- java Script/objects/one.js
+++ java Script/objects/one.js
@@ -8,3 +8,11 @@ emplye.location ="kuppam",
 console.log(emplye)
 delete emplye.location
 console.log(emplye)
+emplye.ename="manu"
+emplye.ename1="manu"
+emplye.ename2="manu"
+emplye.ename3="manu"
+console.log(emplye)
+delete emplye.ename
+console.log(emplye)
+
