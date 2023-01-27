734f725daf103abc1fe6eed84f61679fc32efd82
unknown <iambmanohar@gmail.com>
2023-01-27 20:46:19 +0530

    data

--- java Script/data types/four.js
+++ java Script/data types/four.js
@@ -1,3 +1,15 @@
-let salary =74000;
+/* let salary =74000;
 let message = 75000 > salary ? "eligible for marriage" : "no";
-console.log (message);
+console.log (message); */
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+console.log(reverseString('hello'));
+reverseString('manohar');
+console.log(reverseString('manohar'));
