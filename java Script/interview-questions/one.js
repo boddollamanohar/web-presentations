af63dcf277fd8b1b616511ca97897b46339472ef
unknown <iambmanohar@gmail.com>
2023-04-12 18:45:58 +0530

    data

---
+++ java Script/interview-questions/one.js
@@ -0,0 +1,11 @@
+// var a =10;
+// b=20;
+// c = a +b
+// console.log(typeof (a))
+// undifine
+// var a 
+// console.log(typeof (a))
+//  var  a = 100
+//  console.log(typeof (a+100))
+
+var a= 1
---
+++ java Script/interview-questions/two.js
@@ -0,0 +1,9 @@
+var tableOf = 5
+for (var i =1; i<=20 ; i++)
+
+console.log(tableOf + "*" + i + "=" + tableOf * i) 
+
+
+/* let n = 5;
+    for (let i = 1; i <= 10; ++i)
+        console.log(n + " * " + i + " = " + n * i); */
